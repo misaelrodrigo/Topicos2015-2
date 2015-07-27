ac67e3265b9942f7edce52b95467431c36d4b8ac
Misael <misaelrodrigo@hotmail.com>
2015-07-27 20:54:49 -0300

    commit inicial

---
+++ SpaceInvaders/desktop/src/br/edu/grupointegrado/SpaceInvaders/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package br.edu.grupointegrado.SpaceInvaders.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import br.edu.grupointegrado.SpaceInvaders.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
